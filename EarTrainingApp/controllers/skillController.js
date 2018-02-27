0f48188c3379944da354fecbd52b5660ee643a84
Luyi Bai <Pnctsrc@users.noreply.github.com>
2018-02-26 19:49:58 -0500

    Nested skill levels

--- EarTrainingApp/controllers/skillController.js
+++ EarTrainingApp/controllers/skillController.js
@@ -22,11 +22,38 @@ exports.index = function (req, res) {
 };
 
 // Display list of all Skills.
+//function fetch_levels(current_level, level, skill_list, result) {
+//    //update level
+//    current_level.level = level;
+//    level++;
+//    result.push(current_level);
+
+//    if (current_level.sub_skills) {
+//        for (let sub_skill of current_level.sub_skills) {
+//            for (let skill of skill_list) {
+//                if (sub_skill.id == skill._id) {
+//                    fetch_levels(skill, level, skill_list, result);
+//                }
+//            }
+//        }
+//    }
+//}
+
 exports.skill_list = function (req, res, next) {
     Skill.find({}, 'name parent description')
         .exec(function (err, skill_list) {
             if (err) { return next(err); }
             //Successful, so render
+
+            ////display as nested list
+            //var sorted_list = [];
+
+            //for (let skill of skill_list) {
+            //    if (!skill.parent) {
+            //        fetch_levels(skill, 0, skill_list, sorted_list);
+            //    }
+            //}
+
             res.render('skill_list', { title: 'Skill List', skill_list: skill_list });
         });
 };
