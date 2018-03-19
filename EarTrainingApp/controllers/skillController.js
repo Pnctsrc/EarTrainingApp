a221b7ff4d5c391306d2d77850b6436a219d86f4
Luyi Bai <Pnctsrc@users.noreply.github.com>
2018-03-19 00:16:41 -0400

    Controller for skill_form POST

--- EarTrainingApp/controllers/skillController.js
+++ EarTrainingApp/controllers/skillController.js
@@ -6,6 +6,7 @@ var async = require('async');
 var mongoose = require('mongoose');
 
 var fetch_skill_levels = require('../utils/skill_util').fetch_skill_levels;
+var _ = require('../utils/underscore');
 
 exports.index = function (req, res) {
     async.parallel({
@@ -65,8 +66,143 @@ exports.skill_create_get = function (req, res) {
 };
 
 // Handle Skill create on POST.
-exports.skill_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Skill create POST');
+exports.skill_create_post = function (req, res, next) {
+    const skill_name = req.body.skill_name;
+    const skill_description = req.body.skill_description;
+    const skill_parent = req.body.skill_parent;
+    const sep_skill_name = req.body.sep_skill_name;
+    const sep_skill_description = req.body.sep_skill_description;
+    const skill_children = req.body.skill_children;
+    const skill_requirements = req.body.skill_requirements;
+    const if_replace_parent = req.body.if_replace_parent == 1;
+
+    const new_skill = {
+        name: skill_name,
+        description: skill_description,
+    }
+
+    if (skill_parent) {
+        new_skill.parent = skill_parent;
+    }
+
+    if (skill_requirements) {
+        new_skill.requirements = skill_requirements;
+    }
+
+    const functions = [];
+
+    //save the skill
+    functions.push(function (callback) {
+        var skill = new Skill(new_skill);
+        skill.save(function (err, skill_doc) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, skill_doc, null);
+            }
+        });
+    })
+
+    //save the separate skill
+    if (sep_skill_name) {
+        if (if_replace_parent) {
+            //modify the parent of the affected questions
+            functions.push(function (skill_doc, arg, callback) {
+                Question.updateMany({ parent: skill_doc.prent }, { parent: skill_doc._id }, function (err) {
+                    if (err) {
+                        callback(err, null);
+                    } else {
+                        callback(null, skill_doc, null);
+                    }
+                })
+            })
+        } else {
+            //find the skill to which the affected questions belongs
+            functions.push(function (skill_doc, arg, callback) {
+                Skill.findById(skill_doc.parent, "requirements", function (err, original_parent_doc) {
+                    if (err) {
+                        callback(err, null);
+                    } else {
+                        callback(null, skill_doc, original_parent_doc);
+                    }
+                })
+            })
+
+            //create and save the separate skill
+            functions.push(function (skill_doc, original_parent_doc, callback) {
+                const sep_skill = {
+                    name: sep_skill_name,
+                    description: sep_skill_description,
+                    parent: skill_doc.parent,
+                    requirements: original_parent_doc.requirements
+                }
+
+                var skill = new Skill(sep_skill);
+                skill.save(function (err, sep_skill_doc) {
+                    if (err) {
+                        callback(err, null);
+                    } else {
+                        callback(null, skill_doc, sep_skill_doc);
+                    }
+                })
+            })
+
+            //modify the parent of the affected questions
+            functions.push(function (skill_doc, sep_skill_doc, callback) {
+                Question.updateMany({ skill: skill_doc.parent }, { skill: sep_skill_doc._id }, function (err) {
+                    if (err) {
+                        callback(err, null);
+                    } else {
+                        callback(null, skill_doc, sep_skill_doc);
+                    }
+                })
+            })
+        }
+    }
+
+    //handle children
+    if (skill_children) {
+        functions.push(function (skill_doc, sep_skill_doc, callback) {
+            Skill.updateMany({ _id: { $in: skill_children } }, { parent: skill_doc._id }, function (err) {
+                if (err) {
+                    callback(err, null);
+                } else {
+                    callback(null, skill_doc, sep_skill_doc);
+                }
+            })
+        })
+    }
+
+    async.waterfall(functions, function (err, skill_doc, sep_skill_doc) {
+        if (err) {
+            console.log("something wrong!");
+            console.log("delete all new skills;")
+            async.parallel([
+                function (callback) {
+                    if (skill_doc) {
+                        Skill.remove({ _id: skill_doc._id }, callback);
+                    } else {
+                        callback(null, 'done');
+                    }
+                },
+                function (callback) {
+                    if (sep_skill_doc) {
+                        Skill.remove({ _id: sep_skill_doc._id }, callback);
+                    } else {
+                        callback(null, 'done');
+                    }
+                }
+            ], function (error) {
+                if (error) {
+                    return next(error);
+                } else {
+                    return next(err);
+                }
+            })
+        } else {
+            res.redirect(skill_doc.url);
+        }
+    })
 };
 
 // Display Skill delete form on GET.
