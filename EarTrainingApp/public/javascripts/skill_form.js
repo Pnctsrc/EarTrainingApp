5ec76c57f92786d18065f4e73141e96b0ee04629
Luyi Bai <Pnctsrc@users.noreply.github.com>
2018-04-07 02:59:46 -0400

    Finished client validation on skill_form page

--- EarTrainingApp/public/javascripts/skill_form.js
+++ EarTrainingApp/public/javascripts/skill_form.js
@@ -45,6 +45,20 @@ var app = new Vue({
         if ($("div.parent-box label[disabled-label] input[type=radio]:checked")[0]) {
             app.current_parent = '';
         }
+
+        //uncheck disabled requirements
+        if ($("div.req-box label[disabled-label] input[type=checkbox]:checked")[0]) {
+            $("div.req-box label[disabled-label] input[type=checkbox]").prop("checked", false);
+            const current_req = app.requirements;
+
+            for (var element of $("div.req-box label[disabled-label] input[type=checkbox]")) {
+                if (current_req[$(element).val()]) {
+                    delete current_req[$(element).val()];
+                }
+            }
+
+            app.requirements = current_req;
+        }
     },
     methods: {
         check_parent: function (event) {
@@ -77,6 +91,17 @@ var app = new Vue({
         check_none: function (event) {
             app.current_parent_index = -1;
         },
+        check_req: function (event) {
+            const req_index = $(event.target.parentNode.parentNode).index();
+            const current_req = app.requirements;
+            if (current_req[app.skill_list[req_index]._id]) {
+                delete current_req[app.skill_list[req_index]._id]
+            } else {
+                current_req[app.skill_list[req_index]._id] = app.skill_list[req_index];
+            }
+            app.requirements = {};
+            app.requirements = current_req;
+        },
         generate_dashes: function (index) {
             var dashes = '';
             for (var i = 0; i < app.skill_list[index].level * 4; i++) {
@@ -84,7 +109,7 @@ var app = new Vue({
             }
             return dashes;
         },
-        if_ancestor: function (index) {
+        if_ancestor_parent: function (index) {
             if (app.current_parent_index == -1) return false;
 
             const current_parent = app.skill_list[app.current_parent_index];
@@ -103,13 +128,68 @@ var app = new Vue({
 
             return false;
         },
-        if_descendant: function (index) {
+        if_ancestor_children: function (index) {
+            for (var child in app.children) {
+                const current_req = app.children[child];
+
+                var current_doc = current_req;
+                if (!current_doc.parent) {
+                    if (current_doc._id === app.skill_list[index]._id) return true;
+                }
+                while (current_doc.parent) {
+                    if (current_doc._id === app.skill_list[index]._id) return true;
+                    current_doc = current_doc.parent;
+                }
+                if (!current_doc.parent) {
+                    if (current_doc._id === app.skill_list[index]._id) return true;
+                }
+            }
+
+            return false;
+        },
+        if_ancestor_req: function (index) {
+            for (var req in app.requirements) {
+                const current_req = app.requirements[req];
+
+                var current_doc = current_req;
+                if (!current_doc.parent) {
+                    if (current_doc._id === app.skill_list[index]._id) return true;
+                }
+                while (current_doc.parent) {
+                    if (current_doc._id === app.skill_list[index]._id) return true;
+                    current_doc = current_doc.parent;
+                }
+                if (!current_doc.parent) {
+                    if (current_doc._id === app.skill_list[index]._id) return true;
+                }
+            }
+
+            return false;
+        },
+        if_descendant_parent: function (index) {
+            const current_parent = app.skill_list[app.current_parent_index];
+            if (current_parent._id == app.skill_list[index]._id) return true;
+
+            const current_parent_index = $($("div.parent-box input[value=\"" + current_parent._id + "\"]")[0].parentNode.parentNode).index() - 1;
+
+            var current_lookup_index = current_parent_index + 1;
+            if (current_lookup_index >= app.skill_list.length) continue outer_loop;
+            var current_lookup_req = app.skill_list[current_lookup_index];
+            while (current_lookup_req.level > current_parent.level) {
+                if (current_lookup_req._id == app.skill_list[index]._id) return true;
+                current_lookup_index++;
+                if (current_parent_index >= app.skill_list.length) continue outer_loop;
+                current_lookup_req = app.skill_list[current_lookup_index];
+            }
+
+            return false;
+        },
+        if_descendant_children: function (index, if_inclusive) {
             outer_loop: for (var child in app.children) {
                 const current_child = app.children[child];
-                if (current_child._id == app.skill_list[index]._id) return true;
+                if (current_child._id == app.skill_list[index]._id && if_inclusive) return true;
 
                 const current_child_index = $($("div.children-box input[value=\"" + current_child._id + "\"]")[0].parentNode.parentNode).index();
-                const current_child_level = current_child.level;
 
                 var current_lookup_index = current_child_index + 1;
                 if (current_lookup_index >= app.skill_list.length) continue outer_loop;
@@ -122,6 +202,43 @@ var app = new Vue({
                 }
             }
 
+            return false;
+        },
+        if_descendant_requirements: function (index, if_inclusive) {
+            outer_loop: for (var req in app.requirements) {
+                const current_req = app.requirements[req];
+                if (current_req._id == app.skill_list[index]._id && if_inclusive) return true;
+
+                const current_req_index = $($("div.req-box input[value=\"" + current_req._id + "\"]")[0].parentNode.parentNode).index();
+
+                var current_lookup_index = current_req_index + 1;
+                if (current_lookup_index >= app.skill_list.length) continue outer_loop;
+                var current_lookup_req = app.skill_list[current_lookup_index];
+                while (current_lookup_req.level > current_req.level) {
+                    if (current_lookup_req._id == app.skill_list[index]._id) return true;
+                    current_lookup_index++;
+                    if (current_req_index >= app.skill_list.length) continue outer_loop;
+                    current_lookup_req = app.skill_list[current_lookup_index];
+                }
+            }
+
+            return false;
+        },
+        if_disable_parent: function (index) {
+            if (app.if_descendant_children(index, true)) return true;
+            if (app.if_descendant_requirements(index, true)) return true;
+            return false;
+        },
+        if_disable_children: function (index) {
+            if (app.if_ancestor_parent(index)) return true;
+            if (app.if_descendant_children(index, false)) return true;
+            if (app.if_descendant_requirements(index, true)) return true;
+            return false;
+        },
+        if_disable_req: function (index) {
+            if (app.if_ancestor_parent(index)) return true;
+            if (app.if_descendant_children(index, true)) return true;
+            if (app.if_descendant_requirements(index, false)) return true;
             return false;
         },
     },
