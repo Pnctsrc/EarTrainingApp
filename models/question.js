9fe2ba22738db26f5f7605a951d8c703b88bc4b2
Luyi Bai <pnctsrc@gmail.com>
2018-07-17 04:23:17 -0400

    Fixed question model

--- models/question.js
+++ models/question.js
@@ -30,10 +30,10 @@ var QuestionSchema = new Schema(
             ref: 'User',
             required: true,
         },
-        tags: {
-            type: [Schema.ObjectId],
+        tags: [{
+            type: Schema.ObjectId,
             ref: 'Tag',
-        },
+        }],
     },
     {
         toJSON: {
